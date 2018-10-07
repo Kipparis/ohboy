a7c5072986c2a5141a4897190aa81b5b27338b50
Kipparis <keepintine@gmail.com>
2018-10-07 11:40:23 +0300

    Changed pills

--- pills/pills.sh
+++ pills/pills.sh
@@ -1,10 +1,27 @@
 #!/bin/zsh
 
-echo "hello world"
+now=$(date +%s)
+#echo $now
 
+past=$(date +%s --date "2018-09-14")
+#echo $past
 
-value=`cat current.txt`
+value1=125
+value2=150
+#echo $value
 
-while read line; do
-	echo $line
-done < current.txt
+difference=$(($now-$past))
+#echo $difference
+
+days=$(($difference/(3600*24)))
+#echo $days
+
+#echo $(expr $days % 2)
+if [ $(expr $days % 2) -eq 0 ]
+then
+	echo $value2
+elif [ $(expr $days % 2) -eq 1 ]
+then	
+	echo $value1
+fi
+
